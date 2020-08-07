f90fd332e587d941333367aeef37aad8915ee90c
Kamaleri <YusmanBin.Kamaleri@fhi.no>
2020-08-07 11:22:12 +0100

    Add multiple change function

--- R/globals.R
+++ R/globals.R
@@ -1,2 +1,2 @@
 ## ignore warning on no visible binding
-utils::globalVariables(c("curr", "currName", "new", "old", "prev", "prevName"))
+utils::globalVariables(c("curr", "currName", "new", "old", "prev", "prevName", "code"))
