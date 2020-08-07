bf4cfee1aff09bcec9b6e1a9d0087d2f9b215bd2
Kamaleri <YusmanBin.Kamaleri@fhi.no>
2020-08-07 10:14:50 +0100

    Restrict input files

--- R/add_change.R
+++ R/add_change.R
@@ -12,113 +12,135 @@
 #'   \item code   : Sti til kodefilen hvis \code{raw = FALSE}
 #' }
 #'
-#' @param filegeo CSV fil for alle geokoder
-#' @param filechg Excel fil for kode endringer
+#' @param grep.file Felles navn til filene. Funker med bare noen ord eg. "jan2019"
+#' @param grep.change Navn til endringsfil eg. "change"
 #' @param year    Året geokoder gjelder
 #' @param type    Hvilket nivå f.eks fylke, kommune osv.
-#' @param fpath   Sti til mappen for filene ligger
-#' @param grep.file Navn til geokode fil. Funker med bare noen ord eg. "jan2019"
-#' @param grep.change Navn til endringsfil eg. "change"
-#' @param raw     Hvis FALSE så er \code{filegeo} og \code{filechg} en ferdig
-#' laget object i .GlobalEnv dvs. ikke er CSV og Excel fil
+#' @param folder.path   Sti til mappen hvor filene ligger
+#' @param filegeo CSV fil for alle geokoder
+#' @param filechg Excel fil for kode endringer
+#' @param raw     If TRUE then specify direct filename in \code{filegeo} and \code{filechg}
 #'
 #' @import data.table
 #' @export
 
 
-add_change <- function(filegeo = NULL,
-                       filechg = NULL,
-                       year = NULL,
-                       type = "land",
-                       fpath = NULL,
+add_change <- function(
                        grep.file = NULL,
                        grep.change = NULL,
-                       raw = TRUE){
+                       year = NULL,
+                       type = c("land",
+                                "fylke",
+                                "kommune",
+                                "bydel",
+                                "grunnkrets"),
+                       folder.path = NULL,
+                       filegeo = NULL,
+                       filechg = NULL,
+                       raw = FALSE
+                       ){
 
 
-    if (is.null(year)) {stop("Mangler år")}
+  if (is.null(year)) {stop("Missing year")}
 
-    if (!is.null(filegeo) & !is.null(grep.file))
-    {stop("Bare en av dem trenger info ie. filegeo og grep.file")}
+  if (!is.null(filegeo) & !is.null(grep.file))
+                         {stop("Only one of them need info ie. filegeo or grep.file")}
 
-    ssb <- select_ssb(grep.file = grep.file, grep.change, fpath = fpath)
+  ## Files
+  if (raw){
 
-    filegeo <- ssb$allfile
-    filechg <- ssb$chgfile
+    fileChg <- filechg
+    fileGeo <- filegeo
 
-    ## Files
-    if (!is.null(fpath)){
-        filePath <- normalizePath(fpath, winslash = "/")
+  } else {
 
-        fileNew <- fpath(filePath, filegeo) #geo New
-        fileChg <- fpath(filePath, filechg) #geo Change
-    } else {
-        fileNew <- filegeo
-        fileChg <- filechg
-    }
+    ssb <- select_ssb(grep.file = grep.file,
+                      grep.change = grep.change,
+                      folder.path = folder.path)
+
+    fileGeo <- ssb$allfile
+    fileChg <- ssb$chgfile
+
+  }
+
+  fileErr <- length(fileGeo) + length(fileChg)
+  if (fileErr != 2) {stop("File not found or too many! run `select_ssb()`")}
+
+  ## Changes files - must be an Excel file
+  ## -------------------------------------
+  xlTbl <- readxl::read_excel(fileChg)
+
+  names(xlTbl) <- c("new", "old")
+  setDT(xlTbl)
+
+  expNum <- switch(type,
+                   "kommune" = "[^0-9]+",
+                   "grunnkrets" = "\\s.*",
+                   "fylke" = "[^0-9]+",
+                   "[^0-9]\\s.*")
+
+  expName <- switch(type,
+                    "kommune" = "\\d+\\D[^\\s]",
+                    "grunnkrets" = "[^A-Za-z]",
+                    "[^A-Za-z]")
+
+  ## Extract code and name separately
+  xlTbl[!is.na(new), curr := as.numeric(gsub(expNum, "", new))]
+  xlTbl[!is.na(new), currName := gsub(expName, "", new)]
+  xlTbl[!is.na(old), prev := as.numeric(gsub(expNum, "", old))]
+  xlTbl[!is.na(old), prevName := gsub(expName, "", old)]
+  xlTbl[, year := year, ]
+
+  ## replace missing string with last observed carried forward (locf)
+  setnafill(xlTbl, type = "locf", cols = "curr") #only for numeric
+  ## For string
+  while(length(ind <- which(is.na(xlTbl$currName))) > 0){
+    xlTbl$currName[ind] <- xlTbl$currName[ind - 1]
+  }
+
+  xlTbl[, c("new", "old") := NULL]
+
+  ## Current Geo - must be a CSV file
+  ## --------------------------------
+  dt <- data.table::fread(fileGeo, fill = TRUE)
+
+  mainCols <- c("code", "name")
+  nCols <- names(dt)
+  same <- identical(mainCols, nCols)
+  ##keep only Code and Name
+  if (same == 0){
+    dt[, setdiff(names(dt), mainCols) := NULL]
+  }
 
 
-    ## Geo change
-    ## Use Excel for changes file
-    if (raw){
-        xlTbl <- readxl::read_excel(fileChg)
-    } else {
-        xlTbl <- filechg
-    }
+  ## Merge everything
+  ## ----------------
+  DT <- xlTbl[dt, on = c(curr = "code")]
+  DT[, currName := NULL]
+  setnames(DT, "curr", "code")
+  otherCols <- setdiff(names(DT), mainCols)
+  setcolorder(DT, c(mainCols, otherCols))
 
-    names(xlTbl) <- c("new", "old")
-    setDT(xlTbl)
+  list(DT = DT[], xl = xlTbl[], change = fileChg, code = fileGeo)
 
-    expNum <- switch(type,
-                     "kommune" = "[^0-9]+",
-                     "grunnkrets" = "\\s.*",
-                     "fylke" = "[^0-9]+",
-                     "[^0-9]\\s.*")
+}
 
-    expName <- switch(type,
-                      "kommune" = "\\d+\\D[^\\s]",
-                      "grunnkrets" = "[^A-Za-z]",
-                      "[^A-Za-z]")
 
-    ## Extract code and name separately
-    xlTbl[!is.na(new), curr := as.numeric(gsub(expNum, "", new))]
-    xlTbl[!is.na(new), currName := gsub(expName, "", new)]
-    xlTbl[!is.na(old), prev := as.numeric(gsub(expNum, "", old))]
-    xlTbl[!is.na(old), prevName := gsub(expName, "", old)]
-    xlTbl[, year := year, ]
+#' Select files
+#'
+#' Use \code{regex} to select files
+#' @inheritParams add_change
+#'
+#' @export
 
-    ## replace missing string with last observed carried forward (locf)
-    setnafill(xlTbl, type = "locf", cols = "curr") #only for numeric
-    ## For string
-    while(length(ind <- which(is.na(xlTbl$currName))) > 0){
-        xlTbl$currName[ind] <- xlTbl$currName[ind - 1]
-    }
+select_ssb <- function(grep.file = NULL,
+                       grep.change = NULL,
+                       folder.path = NULL){
 
-    xlTbl[, c("new", "old") := NULL]
-
-    mainCols <- c("code", "name")
-
-    ## New geo
-    if (raw){
-        dt <- data.table::fread(fileNew, fill = TRUE)
-    } else {
-        dt <- filegeo
-    }
-
-    nCols <- names(dt)
-    same <- identical(mainCols, nCols)
-    ##keep only Code and Name
-    if (same == 0){
-        dt[, setdiff(names(dt), mainCols) := NULL]
-    }
-
-    ## Merge
-    DT <- xlTbl[dt, on = c(curr = "code")]
-    DT[, currName := NULL]
-    setnames(DT, "curr", "code")
-    otherCols <- setdiff(names(DT), mainCols)
-    setcolorder(DT, c(mainCols, otherCols))
-
-    list(DT = DT[], xl = xlTbl[], change = fileChg, code = fileNew)
-
+  files <- fs::dir_ls(folder.path)
+  filInd <- grep(grep.file, files, ignore.case = TRUE)
+  chgInd <- grep(grep.change, files[filInd])
+  chgFil <- files[filInd][chgInd]
+  codeList <- files[filInd][-chgInd]
+  list(chgfile = chgFil, allfile = codeList)
 }
