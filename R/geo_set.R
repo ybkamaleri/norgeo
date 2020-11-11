9e602dab49b2dfd18e339b3b7619362511a1ef0e
Yusman Kamaleri <ybkamaleri@users.noreply.github.com>
2020-11-11 17:06:23 +0100

    Download code list with geo_list
    This function is using API from SSB

--- R/geo_set.R
+++ R/geo_set.R
@@ -28,8 +28,7 @@
 geo_set <- function(grep.file = NULL,
                     grep.change = NULL,
                     year = NULL,
-                    type = c("land",
-                             "fylke",
+                    type = c("fylke",
                              "kommune",
                              "bydel",
                              "grunnkrets"),
@@ -38,102 +37,103 @@ geo_set <- function(grep.file = NULL,
                     filechg = NULL
                     ){
 
+  type <- match.arg(type)
+  
+  if (is.null(year)) {stop("Missing year")}
 
-    if (is.null(year)) {stop("Missing year")}
+  if (!is.null(filegeo) & !is.null(grep.file))
+                      {stop("Only one of them need info ie. filegeo or grep.file")}
 
-    if (!is.null(filegeo) & !is.null(grep.file))
-    {stop("Only one of them need info ie. filegeo or grep.file")}
+  raw = FALSE
+  if (!is.null(filegeo)) {
+    fileGeo = filegeo
+    raw = TRUE
+  }
+  
+  if (!is.null(filechg)) {
+    fileChg = filechg
+    raw = TRUE
+  }
+  
+  ## Files
+  if (raw){
 
-    raw = FALSE
-    if (!is.null(filegeo)) {
-        fileGeo = filegeo
-        raw = TRUE
-    }
-    
-    if (!is.null(filechg)) {
-        fileChg = filechg
-        raw = TRUE
-    }
-    
-    ## Files
-    if (raw){
+    fileChg <- filechg
+    fileGeo <- filegeo
 
-        fileChg <- filechg
-        fileGeo <- filegeo
+  } else {
 
-    } else {
+    ssb <- select_ssb(grep.file = grep.file,
+                      grep.change = grep.change,
+                      folder.path = folder.path)
 
-        ssb <- select_ssb(grep.file = grep.file,
-                          grep.change = grep.change,
-                          folder.path = folder.path)
-
-        fileGeo <- ssb$allfile
-        fileChg <- ssb$chgfile
-    }
-
-
-    fileErr <- length(fileGeo) + length(fileChg)
-    if (fileErr != 2) {stop("File not found or too many! run `select_ssb()`")}
-    
-    ## Changes files - must be an Excel file
-    ## -------------------------------------
-    xlTbl <- readxl::read_excel(fileChg)
-
-    names(xlTbl) <- c("new", "old")
-    setDT(xlTbl)
-
-    expNum <- switch(type,
-                     "kommune" = "[^0-9]+",
-                     "grunnkrets" = "\\s.*",
-                     "fylke" = "[^0-9]+",
-                     "[^0-9]\\s.*")
-
-    expName <- switch(type,
-                      "kommune" = "\\d+\\D[^\\s]",
-                      "grunnkrets" = "[^A-Za-z]",
-                      "[^A-Za-z]")
-
-    ## Extract code and name separately
-    xlTbl[!is.na(new), curr := as.numeric(gsub(expNum, "", new))]
-    xlTbl[!is.na(new), currName := gsub(expName, "", new)]
-    xlTbl[!is.na(old), prev := as.numeric(gsub(expNum, "", old))]
-    xlTbl[!is.na(old), prevName := gsub(expName, "", old)]
-    xlTbl[, year := year, ]
-
-    ## replace missing string with last observed carried forward (locf)
-    data.table::setnafill(xlTbl, type = "locf", cols = "curr") #only for numeric
-    ## For string
-    while(length(ind <- which(is.na(xlTbl$currName))) > 0){
-        xlTbl$currName[ind] <- xlTbl$currName[ind - 1]
-    }
-
-    xlTbl[, c("new", "old") := NULL]
-
-    ## Current Geo - must be a CSV file
-    ## --------------------------------
-    dt <- data.table::fread(fileGeo, fill = TRUE)
-
-    mainCols <- c("code", "name")
-    nCols <- names(dt)
-    same <- identical(mainCols, nCols)
-    ##keep only Code and Name
-    if (same == 0){
-        dt[, setdiff(names(dt), mainCols) := NULL]
-    }
+    fileGeo <- ssb$allfile
+    fileChg <- ssb$chgfile
+  }
 
 
-    ## Merge everything
-    ## ----------------
-    DT <- xlTbl[dt, on = c(curr = "code")]
-    DT[, currName := NULL]
-    setnames(DT, "curr", "code")
-    otherCols <- setdiff(names(DT), mainCols)
-    setcolorder(DT, c(mainCols, otherCols))
+  fileErr <- length(fileGeo) + length(fileChg)
+  if (fileErr != 2) {stop("File not found or too many! run `select_ssb()`")}
+  
+  ## Changes files - must be an Excel file
+  ## -------------------------------------
+  xlTbl <- readxl::read_excel(fileChg)
 
-    list(data = DT[],
-         xl = xlTbl[],
-         change = fileChg,
-         code = fileGeo,
-         type = type)
+  names(xlTbl) <- c("new", "old")
+  setDT(xlTbl)
+
+  expNum <- switch(type,
+                   "kommune" = "[^0-9]+",
+                   "grunnkrets" = "\\s.*",
+                   "fylke" = "[^0-9]+",
+                   "[^0-9]\\s.*")
+
+  expName <- switch(type,
+                    "kommune" = "\\d+\\D[^\\s]",
+                    "grunnkrets" = "[^A-Za-z]",
+                    "[^A-Za-z]")
+
+  ## Extract code and name separately
+  xlTbl[!is.na(new), curr := as.numeric(gsub(expNum, "", new))]
+  xlTbl[!is.na(new), currName := gsub(expName, "", new)]
+  xlTbl[!is.na(old), prev := as.numeric(gsub(expNum, "", old))]
+  xlTbl[!is.na(old), prevName := gsub(expName, "", old)]
+  xlTbl[, year := year, ]
+
+  ## replace missing string with last observed carried forward (locf)
+  data.table::setnafill(xlTbl, type = "locf", cols = "curr") #only for numeric
+  ## For string
+  while(length(ind <- which(is.na(xlTbl$currName))) > 0){
+    xlTbl$currName[ind] <- xlTbl$currName[ind - 1]
+  }
+
+  xlTbl[, c("new", "old") := NULL]
+
+  ## Current Geo - must be a CSV file
+  ## --------------------------------
+  dt <- data.table::fread(fileGeo, fill = TRUE)
+
+  mainCols <- c("code", "name")
+  nCols <- names(dt)
+  same <- identical(mainCols, nCols)
+  ##keep only Code and Name
+  if (same == 0){
+    dt[, setdiff(names(dt), mainCols) := NULL]
+  }
+
+
+  ## Merge everything
+  ## ----------------
+  DT <- xlTbl[dt, on = c(curr = "code")]
+  DT[, currName := NULL]
+  setnames(DT, "curr", "code")
+  otherCols <- setdiff(names(DT), mainCols)
+  setcolorder(DT, c(mainCols, otherCols))
+
+  list(data = DT[],
+       xl = xlTbl[],
+       change = fileChg,
+       code = fileGeo,
+       type = type)
 
 }
---
+++ R/get-list.R
@@ -0,0 +1,68 @@
+#' Get the list of geographical levels
+#'
+#' This function will download the codes of selected geographical levels via API.
+#'
+#' @param from Year from
+#' @inheritParams geo_set
+#'
+#' @export
+
+geo_list <- function(type = c("fylke", "kommune", "bydel", "grunnkrets"),
+                     year,
+                     from = NULL){
+
+  type <- match.arg(type)
+  
+  klass <- switch(type,
+                  fylke = 104,
+                  kommune = 131,
+                  bydel = 103,
+                  grunnkrets = 1)
+
+  baseUrl <- "http://data.ssb.no/api/klass/v1/classifications/"
+  klsUrl <- paste0(baseUrl, klass)
+
+  ## choose specific date or fromTo dates
+  kodeUrl <- paste0(klsUrl, ifelse(is.null(from), "/codesAt", "/codes"))
+
+  ## use current dates if year is current year
+  year <- as.character(year)
+  is.curYr <- identical(format(Sys.Date(), "%Y"), year)
+  ## valid codes for current year should NOT start with 01.jan
+  is.errYr <- identical(format(Sys.Date(), "%m-%d"), "01-01")
+  if (is.errYr)
+    warning("OBS!! Run this again tomorrow!")
+  dateTo <- paste0(year, "-01-02")
+
+  if(is.curYr)
+    dateTo <- Sys.Date()
+
+  if (!is.null(from)){
+    dateFrom <- paste0(from, "-01-01")
+    kodeQ <- list(from = dateFrom, to = dateTo)
+  } else {
+    kodeQ <- list(date = dateTo)
+  }
+
+  koGET <- httr::GET(kodeUrl, query = kodeQ)
+  koTxt <- httr::content(koGET, as = "text")
+  koJS <- jsonlite::fromJSON(koTxt)
+
+  koDT <- koJS[["codes"]]
+  data.table::setDT(koDT)
+
+  keepName <-  c("code", "name")
+
+  if (is.null(from)){
+    koDT[, setdiff(names(koDT), keepName) := NULL]
+    koDT[, validTo := dateTo][]
+  } else {
+    indN <- grep("InRequestedRange", names(koDT))
+    valN <- names(koDT)[indN]
+    koDT[, setdiff(names(koDT), c(keepName, valN)) := NULL][]
+    data.table::setnames(koDT, old = valN, new = c("from", "validTo"))
+    data.table::setorderv(koDT, c("validTo", "code"))
+  }
+
+  invisible(koDT)
+}
