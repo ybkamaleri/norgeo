4d205b703519d6dd88cd21f844973120f55028ec
YBK <ybkamaleri@users.noreply.github.com>
2021-05-31 16:15:32 +0200

    Test: Feature track_change first commit
    Codes duplicates when changes happen more than once while keeping
    the new codes eg. Trondheim

---
+++ R/track-change.R
@@ -0,0 +1,78 @@
+#' Track all changes for codes from API
+#'
+
+track_change <- function(type, from, to) {
+  dataApi$dt <- data_current(type, from, to)
+  vecYear <- unique(dataApi$dc$changeOccurred)
+  selYear <- sort(vecYear[vecYear != dataApi$yrMax], decreasing = TRUE)
+
+  for (i in seq_along(selYear)) {
+    DT <- data_merge(dataApi$dt, dataApi$dc, selYear[i])
+    dataApi$dt <- DT
+  }
+
+  data.table::setkey(dataApi$dt, newCode, changeOccurred)
+  ## When nothing changes
+  dataApi$dt[oldCode == newCode, oldCode := NA][]
+}
+
+## Do testing with:
+## dat <- data_current()
+## length(unique(dat$changeOccurred))==2
+data_current <- function(type, from, to) {
+  DT <- get_code(type, from = to)
+
+  data_change(type, from, to)
+
+  yrMax <- max(dataApi$dc$changeOccurred)
+  dataApi$yrMax <- yrMax
+
+  dt <- dataApi$dc[changeOccurred == yrMax][DT, on = c(newCode = "code")]
+
+  ## when no code changes have occurred
+  dt[is.na(oldCode), oldCode := newCode][
+    is.na(changeOccurred), changeOccurred := validTo
+  ][
+    is.na(newName), newName := name
+  ]
+
+  dt[, c("name", "validTo") := NULL][]
+}
+
+## data1 : newest dataset with code changes
+## data2 : older dataset with code changes
+## year  : year for subset
+data_merge <- function(data1, data2, year) {
+
+  ## 1 to 1 merge
+  dtc <- data1[data2[changeOccurred == year], on = c(oldCode = "newCode")]
+  delCols <- c("oldCode", "oldName", "i.newName", "changeOccurred")
+  dtc[, (delCols) := NULL]
+  setnames(
+    dtc,
+    c("i.oldCode", "i.oldName", "i.changeOccurred"),
+    c("oldCode", "oldName", "changeOccurred")
+  )
+
+  ## previous changes
+  dtp <- data1[data2[changeOccurred == year], on = "newCode"]
+  delCols <- c("oldCode", "oldName", "newName", "changeOccurred")
+  dtp[, (delCols) := NULL]
+  setnames(
+    dtp,
+    c("i.oldCode", "i.changeOccurred", "i.oldName", "i.newName"),
+    c("oldCode", "changeOccurred", "oldName", "newName")
+  )
+
+  dtprev <- dtp[newCode != oldCode]
+
+  DTC <- rbindlist(list(dtc, dtprev), use.names = TRUE)
+  dd <- rbindlist(list(data1, DTC), use.names = TRUE)
+}
+
+data_change <- function(type, from, to) {
+  dataApi$dc <- get_change(type, from, to, quiet = TRUE)
+}
+
+
+dataApi <- new.env()
