84df371b0e2272355018729dacc28968c5edc9b4
Yusman Kamaleri <ybkamaleri@gmail.com>
2021-06-02 13:40:09 +0200

    Docs: Use Git Actions for CI

--- memo/start-pkg.R
+++ memo/start-pkg.R
@@ -21,3 +21,8 @@ devtools::test()
 # Run to build the website
 pkgdown::build_site()
 pkgdown::build_news(preview = TRUE)
+
+# Use CI
+usethis::use_git_remote("origin", url = "https://github.com/helseprofil/norgeo.git", overwrite = TRUE)
+usethis::use_github_action_check_standard()
+usethis::use_git_remote("origin", url = "git@work:helseprofil/norgeo.git", overwrite = TRUE)
